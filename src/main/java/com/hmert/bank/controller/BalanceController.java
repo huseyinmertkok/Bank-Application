a8acf590874b5be8558e4a5c6255e2430f475217
huseyinmertkok <huseyinmert.koek@gmail.com>
2023-10-13 14:20:14 +0300

    implement controllers

---
+++ src/main/java/com/hmert/bank/controller/BalanceController.java
@@ -0,0 +1,14 @@
+package com.hmert.bank.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/balance")
+public class BalanceController {
+    @GetMapping
+    public String getBalanceDetails(){
+        return "Balance Details.";
+    }
+}
